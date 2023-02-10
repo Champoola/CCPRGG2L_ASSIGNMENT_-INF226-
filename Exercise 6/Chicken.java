4f784f06376485ee26854340cc03fee946aa468e
Dhan Lloyd Ferrer <119914233+Champoola@users.noreply.github.com>
2023-02-10 16:28:33 +0800

    Add files via upload

---
+++ Exercise 6/Chicken.java
@@ -0,0 +1,13 @@
+public class Chicken implements Animal  {
+
+    public void walk() {
+        System.out.println("Chicken is walking");
+    }
+
+    public void run() {
+        System.out.println("Chicken is Running");
+    }
+
+
+    
+}
