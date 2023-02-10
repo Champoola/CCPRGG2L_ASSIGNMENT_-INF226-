e4b64794c25f79d7241de9b2acae869d9d06f969
Dhan Lloyd Ferrer <119914233+Champoola@users.noreply.github.com>
2023-02-10 14:24:19 +0800

    Add files via upload

---
+++ Exercise 5/Daddy.java
@@ -0,0 +1,10 @@
+ public class Daddy {
+    
+    //Properties
+    protected String surname = "Ferrer";
+
+    protected void showSurname() {
+        System.out.println("My family name is " + surname);
+
+    }
+}
